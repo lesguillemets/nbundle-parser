03733563c5b74f2dd8a3b5321fc86b444014f960
lesguillemets <lesguillemets@users.noreply.github.com>
2016-04-25 00:12:01 +0900

    fancy main

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,11 @@
 import qualified Data.ByteString.Char8 as BC
 import Data.ByteString.Char8 (ByteString)
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
 
 import Vim
 
 main = do
-    putStrLn "hello, world!"
+    home <- getHomeDirectory
+    BC.readFile (home </> ".vimrc") >>=
+        mapM_ (BC.putStrLn . Vim.lineContent) . Vim.toVimLines
